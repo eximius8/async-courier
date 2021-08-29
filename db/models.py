3ba35b4e317d78a6802c1fc2004d97d62ac4c5f5
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-08-30 02:46:44 +0300

    Db works

--- db/models.py
+++ db/models.py
@@ -1,9 +1,17 @@
 from sqlalchemy import Column, Integer, String
 
-from db.config import Base
+from db.config import Base, engine
 
 class Delivery(Base):
     __tablename__ = 'delivery'
 
     id = Column(Integer, primary_key=True)
     status = Column(String, nullable=False)
+
+
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+init_models()
--- main.py
+++ main.py
@@ -1,31 +1,76 @@
-import uvicorn
-from fastapi import FastAPI
-
+from logging import debug
 from typing import List
+from sqlalchemy.sql.expression import join
+from fastapi.encoders import jsonable_encoder
 
-from db.config import async_session
-from db.delivery_dal import DeliveryDAL
-from db.models import Delivery
+import uvicorn
+
+import databases
+import sqlalchemy
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+# SQLAlchemy specific code, as with any other app
+DATABASE_URL = "sqlite:///./test.db"
+# DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+deliveries = sqlalchemy.Table(
+    "deliveries",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("status", sqlalchemy.String),
+)
 
 
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+metadata.create_all(engine)
+
+
+
+class DeliveryIn(BaseModel):
+    status: str
+
+
+class Delivery(BaseModel):
+    id: int
+    status: str
+    
 
 app = FastAPI()
 
 
-@app.post("/deliveries")
-async def create_delivery(status: str):
-    async with async_session() as session:
-        async with session.begin():
-            delivery_dal = DeliveryDAL(session)
-            return await delivery_dal.create_delivery(status)
+@app.on_event("startup")
+async def startup():
+    await database.connect()
 
-@app.get("/deliveries")
-async def get_all_deliveries() -> List[Delivery]:
-    async with async_session() as session:
-        async with session.begin():
-            delivery_dal = DeliveryDAL(session)
-            return await delivery_dal.get_all_deliveries()
 
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.get("/deliveries/", response_model=List[Delivery])
+async def read_deliveries():
+    query = deliveries.select()
+    return await database.fetch_all(query)
+
+
+@app.post("/deliveries/", response_model=Delivery)
+async def create_note(delivery: DeliveryIn):
+    allowed_statuses = ['обрабатывается', 'выполняется', 'доставлено']
+    if delivery.status in allowed_statuses:
+        query = deliveries.insert().values(status=delivery.status)
+        last_record_id = await database.execute(query)
+        return {**delivery.dict(), "id": last_record_id}
+    raise HTTPException(status_code=404, detail="Неверный статус, статус может быть: " 
+                                                + ", ".join(allowed_statuses))
+ 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", port=8000, host='127.0.0.1')
+    uvicorn.run("main:app", port=8000, host='127.0.0.1', debug=True)
